fe3f7d098650c68cdb9d5b8e21cf41b563bbcd4f
nsheremet <Nikita.Sheremet@noveogroup.com>
2013-05-21 17:26:22 +0700

    add id

--- src/main/java/com/git_training/Publisher.java
+++ src/main/java/com/git_training/Publisher.java
@@ -2,9 +2,15 @@ package com.git_training;
 
 public class Publisher {
     private String message;
+    private static Integer id;
 
     public String getMessage () {
         return message;
+    }
+
+    public Integer getId () {
+        return id++;
+
     }
 
     public void setMessage (String message) {
@@ -13,6 +19,6 @@ public class Publisher {
 
     @Override
     public String toString () {
-        return message;
+        return "Pubslisher is:\n" + "\tMessage: " + message + "\n" + "\tId: " + getId();
     }
 }
