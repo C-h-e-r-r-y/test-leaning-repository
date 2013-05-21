533c0110ee3f1f61e6f01fde752a094440c971f5
nsheremet <Nikita.Sheremet@noveogroup.com>
2013-05-21 16:09:26 +0700

    create factory

--- src/main/java/com/git_training/AppRunner.java
+++ src/main/java/com/git_training/AppRunner.java
@@ -1,14 +1,10 @@
 package com.git_training;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 public class AppRunner {
     public static void main (String[] args) {
-        List<Publisher> data = new ArrayList<Publisher>();
-        Publisher publisher = new Publisher();
-        publisher.setMessage("Hello");
-        data.add(publisher);
+        Collection<Publisher> data = PublisherFactory.getInstance().createCollection(3);
         for (Publisher p : data) {
             System.out.println(p);
         }
